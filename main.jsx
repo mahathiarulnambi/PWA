9153005924ed10a8a60d5b378766ba14de19d778
Goutham <gouthamfoxy4@gmail.com>
2025-11-27 11:42:31 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './styles/home.css'
+import './styles/login.css'
+import './styles/profile.css'
+import './styles/dashboard.css'
+
+
+createRoot(document.getElementById('root')).render(<App />)
